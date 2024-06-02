e5cbdf33dad8a9d6dd3f06bcd8adf4d5af9fe006
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2024-06-02 12:03:30 +0400

    feat: Правки docker-compose

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
+    Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
 ];
