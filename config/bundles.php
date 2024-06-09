80b8fa9884f9fb88268c1610242cb7bd9fb02a42
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2024-06-09 13:19:34 +0400

    feat: Добавил PimcoreSimpleBackendSearchBundle

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,5 @@ return [
     Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
     Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle::class => ['all' => true],
     Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle::class => ['all' => true],
+    Pimcore\Bundle\SimpleBackendSearchBundle\PimcoreSimpleBackendSearchBundle::class => ['all' => true],
 ];
