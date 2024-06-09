d2db37945847b99c7ded7d4bb38fadb8a6cd5077
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2024-06-09 10:55:48 +0400

    feat: Добавил бандлы DataHub и DataImporter

--- config/bundles.php
+++ config/bundles.php
@@ -1,6 +1,7 @@
 <?php
-
 return [
     Pimcore\Bundle\TinymceBundle\PimcoreTinymceBundle::class => ['all' => true],
     Pimcore\Bundle\WordExportBundle\PimcoreWordExportBundle::class => ['all' => true],
+    Pimcore\Bundle\DataHubBundle\PimcoreDataHubBundle::class => ['all' => true],
+    Pimcore\Bundle\DataImporterBundle\PimcoreDataImporterBundle::class => ['all' => true],
 ];
